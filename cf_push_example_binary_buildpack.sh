562cc92cad7b533250600342bb96beceeaa55b43
Ralph Meira <rmeira@pivotal.io>
2016-09-16 17:16:14 -0400

    Updated Binary Shell cf_push example

--- cf_push_example_binary_buildpack.sh
+++ cf_push_example_binary_buildpack.sh
@@ -1,5 +1,5 @@
-# rmeira@pivotal.io is already using chess.cfapps.io, so you have to pick another name/URL for the chess app when you cf push it
+# rmeira@pivotal.io is already using chess.cfapps.io, so you have to pick another namei/URL for the chess app when you push it
 # and make sure ./culy is stressing your URL and not the one shown in this example
 # When using this command be aware that ./curly (binary) will be copied from your local directory into PCF and no compilation of curly.c will take place
 
-cf push curly --no-route -b https://github.com/atris/heroku-buildpack-C.git -c "./curly http://rfm-sticky-session.cfapps.io 2 2000" --health-check-type 'none' -i 1  -m 30MB
+cf push curly --no-route -b https://github.com/cloudfoundry/binary-buildpack -c "./curly http://chess.cfapps.io 2 2000" --health-check-type 'none' -i 1  -m 30MB
