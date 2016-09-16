f2f998fced44c9959e010f91a06fddacf86f03bc
Ralph Meira <rmeira@pivotal.io>
2016-09-16 16:45:00 -0400

    Corrected spelling

--- cf_push_example_heroku_buildpack.sh
+++ cf_push_example_heroku_buildpack.sh
@@ -1,4 +1,4 @@
-# rmeira@pivotal.io is already using chess.cfapps.io, so you have to pick another namei/URL for the chess app when you push it
+# rmeira@pivotal.io is already using chess.cfapps.io, so you have to pick another name/URL for the chess app when you cf push it
 # and make sure ./culy is stressing your URL and not the one shown in this example
 
 cf push curly --no-route -b https://github.com/atris/heroku-buildpack-C.git -c "./curly http://chess.cfapps.io 2 2000" --health-check-type 'none' -i 1 -m 30MB
