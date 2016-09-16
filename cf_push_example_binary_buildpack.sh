cf2c19da237184f79a9dedcce8064cbcd0d3e468
Ralph Meira <rmeira@pivotal.io>
2016-09-16 16:44:38 -0400

    Corrected spelling

--- cf_push_example_binary_buildpack.sh
+++ cf_push_example_binary_buildpack.sh
@@ -1,4 +1,4 @@
-# rmeira@pivotal.io is already using chess.cfapps.io, so you have to pick another namei/URL for the chess app when you push it
+# rmeira@pivotal.io is already using chess.cfapps.io, so you have to pick another name/URL for the chess app when you cf push it
 # and make sure ./culy is stressing your URL and not the one shown in this example
 # When using this command be aware that ./curly (binary) will be copied from your local directory into PCF and no compilation of curly.c will take place
 
