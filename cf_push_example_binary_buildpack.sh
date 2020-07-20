379ac61bb56c4454eaa1e86842d06f0fa8aa0c85
Ralph Meira <rmeira@vmware.com>
2020-07-20 12:53:59 -0400

    Update cf_push_example_binary_buildpack.sh

--- cf_push_example_binary_buildpack.sh
+++ cf_push_example_binary_buildpack.sh
@@ -2,4 +2,4 @@
 # and make sure ./culy is stressing your URL and not the one shown in this example
 # When using this command be aware that ./curly (binary) will be copied from your local directory into PCF and no compilation of curly.c will take place
 
-cf push curly --no-route -b https://github.com/cloudfoundry/binary-buildpack -c "./curly http://chess.cfapps.io 0 2000000" --health-check-type 'none' -i 1  -m 30MB
+cf push curly --no-route -b https://github.com/cloudfoundry/binary-buildpack -c "./curly http://chess.cfapps.io 0 2000000" --health-check-type 'none' -i 10  -m 30MB
